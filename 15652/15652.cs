4f2081589b2d02cefd2ee670aa2d1f423025c62f
royun0307 <109058557+royun0307@users.noreply.github.com>
2026-03-08 21:34:36 +0900

    Create 15652.cs

---
+++ 15652/15652.cs
@@ -0,0 +1,44 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+class Program
+{
+    static int n, m;
+    static int[] selected;
+    static StringBuilder sb = new StringBuilder();
+
+    static void Dfs(int depth, int start)
+    {
+        if (depth == m)
+        {
+            for (int i = 0; i < m; i++)
+            {
+                sb.Append(selected[i]).Append(' ');
+            }
+            sb.AppendLine();
+            return;
+        }
+
+        for (int num = start; num <= n; num++)
+        {
+            selected[depth] = num;
+            Dfs(depth + 1, num);
+        }
+    }
+
+    public static void Main()
+    {
+        string[] input = Console.ReadLine()!.Split();
+        n = int.Parse(input[0]);
+        m = int.Parse(input[1]);
+
+        selected = new int[m];
+
+        Dfs(0, 1);
+        
+        Console.WriteLine(sb.ToString());
+    }
+}
