4957430f4e4c431e23d8bcff5bb6aa0f63b02497
royun0307 <109058557+royun0307@users.noreply.github.com>
2026-03-25 19:25:14 +0900

    Create 9251.cs

---
+++ 9251/9251.cs
@@ -0,0 +1,32 @@
+using System;
+
+class Program
+{
+    public static void Main()
+    {
+        string a = Console.ReadLine()!;
+        string b = Console.ReadLine()!;
+
+        int n = a.Length;
+        int m = b.Length;
+
+        int[,] dp = new int[n + 1, m + 1];
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                if (a[i - 1] == b[j - 1])
+                {
+                    dp[i, j] = dp[i - 1, j - 1] + 1;
+                }
+                else
+                {
+                    dp[i, j] = Math.Max(dp[i - 1, j], dp[i, j - 1]);
+                }
+            }
+        }
+
+        Console.WriteLine(dp[n, m]);
+    }
+}
