d6c40e8be892a333f20b0b05a669645877f638ea
royun0307 <109058557+royun0307@users.noreply.github.com>
2026-04-09 23:21:21 +0900

    Create 18108.cs

---
+++ 18108/18108.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    public static void Main()
+    {
+        int y = int.Parse(Console.ReadLine()!);
+        int x = y - 543;
+        Console.WriteLine(x);
+    }
+}
